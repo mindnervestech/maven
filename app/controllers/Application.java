b85add5b19766b4c8f81ac58c233092a268e6994
“abc” <harshad.r.gojare@gmail.com>
2016-07-01 16:06:03 +0530

    edit ch

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -323,11 +323,15 @@ public class Application extends Controller {
 		}
 		
 
-		/*public class Filters extends DefaultHttpFilters {
-		    @Inject public Filters(CORSFilter corsFilter) {
-		        super(corsFilter);
-		    }
-		}*/
+		public static Result preflight(String all) {
+	        response().setHeader("Access-Control-Allow-Origin", "*");
+	        response().setHeader("Allow", "*");
+	        response().setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+	        response().setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Referer, User-Agent");
+	        return ok();
+	    }
+		
+		
 		
 		public static Result locationWise(Long locationId){
 			AuthUser user = AuthUser.getOnlyGM();
