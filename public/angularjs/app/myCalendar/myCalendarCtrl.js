7c77d7ff0d9c5e3a159ea1fb515e3e5d273385ca
“abc” <harshad.r.gojare@gmail.com>
2016-07-06 20:21:06 +0530

    cale

--- public/angularjs/app/myCalendar/myCalendarCtrl.js
+++ public/angularjs/app/myCalendar/myCalendarCtrl.js
@@ -1,5 +1,5 @@
 angular.module('newApp')
-.controller('myCalendarCtrl', ['$scope','$http','$location','$filter','$routeParams','$upload', function ($scope,$http,$location,$filter,$routeParams,$upload) {
+.controller('myCalendarCtrl', ['$scope','$http','$location','$filter','$routeParams','$upload','$timeout', function ($scope,$http,$location,$filter,$routeParams,$upload,$timeout) {
 	  //$.fn.Data.checkbox();
 	$scope.eventList = [];
 	$scope.flag = 0;
@@ -11,6 +11,7 @@ angular.module('newApp')
 		angular.forEach(data.calenderTimeTable, function(obj, index){
 			$scope.eventList.push({
 				title:obj.portalName,
+				name:obj.portalName,
 				start: obj.contractDurStartDate+"T"+obj.openTime,
 				end: obj.contractDurStartDate+"T"+obj.closeTime,
 				rendering:'background',
@@ -21,7 +22,10 @@ angular.module('newApp')
 		});
 	
 		console.log($scope.eventList);
-		$scope.initFirstFunction($scope.eventList);
+		
+		//$timeout(function() {
+			$scope.initFirstFunction($scope.eventList);
+		  // }, 10000);
 	});
 	
 	$scope.initFirstFunction = function(eventList){
@@ -34,66 +38,7 @@ angular.module('newApp')
             right: 'agendaWeek,agendaDay'
         },*/
 		
-		$('#calendar').fullCalendar({
-			
-		    defaultView: 'agendaWeek',
-		    events: eventList,
-		    editable: true,
-	        droppable: true, // this allows things to be dropped onto the calendar !!!
-	        drop: function(date, allDay) { // this function is called when something is dropped
-
-	            // retrieve the dropped element's stored Event Object
-	            var originalEventObject = $(this).data('eventObject');
-
-	            // we need to copy it, so that multiple events don't have a reference to the same object
-	            var copiedEventObject = $.extend({}, originalEventObject);
-
-	            // assign it the date that was reported
-	            copiedEventObject.start = date;
-	            copiedEventObject.allDay = allDay;
-
-	            // render the event on the calendar
-	            // the last `true` argument determines if the event "sticks" (http://arshaw.com/fullcalendar/docs/event_rendering/renderEvent/)
-	            $('#calendar').fullCalendar('renderEvent', copiedEventObject, true);
-
-	            // is the "remove after drop" checkbox checked?
-	            if ($('#drop-remove').is(':checked')) {
-	                // if so, remove the element from the "Draggable Events" list
-	                $(this).remove();
-	            }
-
-	        }
-		});
-
-		
-	}
-	
-	
-	/*$('#calendar').fullCalendar({
-		defaultDate: '2016-07-10',
-	    defaultView: 'agendaWeek',
-	    events: [
-	        {
-	            start: '2016-07-02T01:00:00',
-	            end: '2016-07-05T03:00:00',
-	            rendering: 'background',
-	        },
-	        {
-	            start: '2016-11-10T07:00:00',
-	            end: '2016-11-10T09:00:00',
-	            rendering: 'background',
-	        },
-	        {
-	            start: '2016-11-07T07:00:00',
-	            end: '2016-11-08T09:00:00',
-	            rendering: 'background',
-	        }
-	        
-	    ]});
-*/	
-	
-
-	    var eventDrag = function(el){
+		var eventDrag = function(el){
 	        // create an Event Object (http://arshaw.com/fullcalendar/docs/event_data/Event_Object/)
 	        // it doesn't need to have a start or end
 	        var eventObject = {
@@ -111,9 +56,77 @@ angular.module('newApp')
 	        });
 	    };
 
+
 	    $('#external-events div.external-event').each(function() {
 	        eventDrag($(this));
 	    });
+		
+			$('#calendar').fullCalendar({
+				
+			    defaultView: 'agendaWeek',
+			    events: eventList,
+			    editable: true,
+		        droppable: true,
+		        // this allows things to be dropped onto the calendar !!!
+		        drop:function(date, allDay) { // this function is called when something is dropped
+
+		            // retrieve the dropped element's stored Event Object
+		            var originalEventObject = $(this).data('eventObject');
+
+		            // we need to copy it, so that multiple events don't have a reference to the same object
+		            var copiedEventObject = $.extend({}, originalEventObject);
+
+		            // assign it the date that was reported
+		            copiedEventObject.start = date;
+		            copiedEventObject.allDay = allDay;
+
+		            // render the event on the calendar
+		            // the last `true` argument determines if the event "sticks" (http://arshaw.com/fullcalendar/docs/event_rendering/renderEvent/)
+		            $('#calendar').fullCalendar('renderEvent', copiedEventObject, true);
+
+		            // is the "remove after drop" checkbox checked?
+		            if ($('#drop-remove').is(':checked')) {
+		                // if so, remove the element from the "Draggable Events" list
+		                $(this).remove();
+		            }
+
+		        }
+			
+			});
+			
+			
+			
+			 /* var addEvent = function (name) {
+				  console.log("gyyyyyyuuu");
+			        name = name.length === 0 ? "Untitled Event" : name;
+			        var html = $('<div class="external-event label label-default">' + name + '</div>');
+			        $('#event-box').append(html);
+			        eventDrag(html);
+			    };*/
+
+			    /*$('#event-add').on('click', function () {
+			        var name = $('#event-name').val();
+			        addEvent(name);
+			    });
+
+			    $(".page-content").css("padding-bottom", "120px");*/
+		
+		//var element1 = angular.element("<div id='event-box'><div class='external-event col-md-12' style='color: white;background:red;'>kkkk</div></div>");
+		  //$("#showDiv").append(element1);
+			
+			
+
+	}
+	
+
+	
+	
+	
+	
+	
+	
+
+	    
 
 	    /* initialize the calendar
 	     -----------------------------------------------------------------*/
@@ -151,7 +164,7 @@ angular.module('newApp')
 	        }
 	    });
 */
-	    var addEvent = function (name) {
+	  /*  var addEvent = function (name) {
 	        name = name.length === 0 ? "Untitled Event" : name;
 	        var html = $('<div class="external-event label label-default">' + name + '</div>');
 	        $('#event-box').append(html);
@@ -163,7 +176,7 @@ angular.module('newApp')
 	        addEvent(name);
 	    });
 
-	    $(".page-content").css("padding-bottom", "120px");
+	    $(".page-content").css("padding-bottom", "120px");*/
 	
 	
 }]);	
