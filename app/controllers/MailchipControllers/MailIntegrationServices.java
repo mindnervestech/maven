debfc0dd7678032c261bc646f6b24777c2d4fe64
jagbir <jagbir@slicketticket.com>
2016-11-16 19:21:52 +0530

    mailchimp

--- app/controllers/MailchipControllers/MailIntegrationServices.java
+++ app/controllers/MailchipControllers/MailIntegrationServices.java
@@ -224,8 +224,13 @@ public class MailIntegrationServices {
 	}
 	public void unsubscribe(String lastname, String firstname, String email) {
 		initialize();
-		boolean listSubscribe = mcServices.listUnsubscribe(apiKey, listId, email, true, true, true);
-		addUser(lastname,firstname,email);
+		try {
+			boolean listSubscribe = mcServices.listUnsubscribe(apiKey, listId, email, true, true, true);
+			addUser(lastname,firstname,email);
+		} catch (Exception e) {
+			e.printStackTrace();
+			addUser(lastname,firstname,email);
+		}
 	}
 	
 	/*public void sendCampaign(String subject, String text, int templeteId) {
