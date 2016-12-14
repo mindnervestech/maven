4442ece541a61ec1214c6eced427c17d3a01102e
unknown <mindnervestech>
2016-12-14 16:23:06 +0530

    add vm file

---
+++ app/viewmodel/CloseLeadVM.java
@@ -0,0 +1,15 @@
+package viewmodel;
+
+import java.util.List;
+
+public class CloseLeadVM {
+
+	public Long id;
+	public List<String> actionSelectedLead;
+	public List<KeyValueDataVM> customData;
+	public String reasonToCancel;
+	public String action;
+	public Long userNoteId;
+	public String userNote;
+	
+}
