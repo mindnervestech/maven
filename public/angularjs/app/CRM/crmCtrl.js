2cdd10c50683ba793f1a585d78bedecbe3e389b1
unknown <mindnervestech>
2017-01-28 18:36:28 +0530

    check box on the crm table

--- public/angularjs/app/CRM/crmCtrl.js
+++ public/angularjs/app/CRM/crmCtrl.js
@@ -346,9 +346,9 @@ angular.module('newApp')
 						
 					});
 					
-					$scope.gridOptions.columnDefs.push({ name: 'edit', displayName: 'Edit', width:'6%',enableFiltering: false, cellEditableCondition: false, enableSorting: false, enableColumnMenu: false,
+					/*$scope.gridOptions.columnDefs.push({ name: 'edit', displayName: 'Edit', width:'6%',enableFiltering: false, cellEditableCondition: false, enableSorting: false, enableColumnMenu: false,
                                cellTemplate:' <i class="glyphicon glyphicon-edit" ng-click="grid.appScope.editContactsDetail(row)" style="margin-top:7px;margin-left:8px;" title="Edit"></i>&nbsp;&nbsp;&nbsp;<i class="fa fa-trash" title="Remove Contact" ng-click="grid.appScope.deleteContactsDetail(row)" style="margin-top:7px;margin-left:8px;" title="Edit"></i>', 
-                        });
+                        });*/
 					/*$scope.gridOptions.columnDefs.push({ name: 'newsletter', displayName: 'Newsletter',enableFiltering: false, width:'8%', cellEditableCondition: false, enableSorting: false, enableColumnMenu: false,
                         cellTemplate:'<div class="icheck-list"><input type="checkbox" ng-model="row.entity.newsletter" ng-checked:newsletter ng-change="grid.appScope.setAsRead(row.entity.newsletter,row.entity.contactId)" data-checkbox="icheckbox_flat-blue" style="margin-left:42%;"></div>', 
                         });*/
