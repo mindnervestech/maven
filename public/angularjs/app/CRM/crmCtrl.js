14d8691716ca6a76860d1f4d248096bd493e8458
unknown <mindnervestech>
2017-01-28 20:49:21 +0530

    check box on the crm table

--- public/angularjs/app/CRM/crmCtrl.js
+++ public/angularjs/app/CRM/crmCtrl.js
@@ -673,6 +673,7 @@ angular.module('newApp')
 	    			$scope.contactsDetails.customData = $scope.customList;
 	    			 apiserviceCrm.updateContactsData($scope.contactsDetails).then(function(data){
 	    				 $('#contactsModal').modal('hide');
+	    				 $scope.getContactsData();
 	    			 });
      	  		});
 			
